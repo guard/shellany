b561fb597868a6b8feef33d2c82e2d2469072bde
Cezary Baginski <cezary@chronomantic.net>
2014-12-25 02:43:14 +0100

    setup as new gem

---
+++ spec/shellany_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Shellany do
+  it 'has a version number' do
+    expect(Shellany::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
